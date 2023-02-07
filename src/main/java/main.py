2598770635343b3804524f5befe60d14c878d7dd
Xiyu-Serena-Wan <64742214+Xiyu-Serena-Wan@users.noreply.github.com>
2023-02-06 16:10:21 -0800

    add data analysis code

---
+++ src/main/java/main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+threads_df = pd.read_csv("/Users/Xiyu/Downloads/Week3Client/write.csv") # , delimiter=";"
+# print(threads_df.head(5))
+print(threads_df.columns);
+latency = threads_df['Latency']
+# print(latency.describe())
+# print(latency.mean())
+# print(latency.median())
+# print(latency.quantile(0.99))
+# print(latency.max())
+#
+# print(latency.min())
+
+threads_df['x'] = (threads_df['Start Timestamp'] - threads_df['Start Timestamp'][0]) // 1000
+df2 = threads_df.groupby(['x'])['x'].count()
+print(threads_df['x'])
+print(df2)
+df2.plot()
+plt.show()
